3f3e7319bb6d180eb7c023dc2d052ca6f39bc350
christianTDConsulting <christian.berdejo@tdconsulting.es>
2023-11-14 13:59:44 +0100

    scrollable

--- src/app/pages/viewClientes/viewCliente.module.ts
+++ src/app/pages/viewClientes/viewCliente.module.ts
@@ -1,6 +1,6 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TelefonosComponent } from '../../../components/telefonoDialog/telefono/telefonos.component';
+import { TelefonosComponent } from '../../components/telefonoDialog/telefono/telefonos.component';
 import { HttpClientModule } from '@angular/common/http';
 import { TableModule } from 'primeng/table';
 import { ButtonModule } from 'primeng/button';
@@ -22,7 +22,7 @@ import { CardModule } from 'primeng/card';
 import { SelectButtonModule } from 'primeng/selectbutton';
 import { InputNumberModule } from 'primeng/inputnumber';
 import { CalendarModule } from 'primeng/calendar';
-import { ConsumoComponent } from '../../../components/telefonoDialog/telefono/consumos/consumo.component';
+import { ConsumoComponent } from '../../components/telefonoDialog/telefono/consumos/consumo.component';
 import { ConfirmDialogModule } from 'primeng/confirmdialog';
 import { ConfirmationService } from 'primeng/api';
 import { InputMaskModule } from 'primeng/inputmask';
