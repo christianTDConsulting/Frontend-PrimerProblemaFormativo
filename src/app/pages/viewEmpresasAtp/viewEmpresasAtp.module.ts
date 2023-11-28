e42e935568eeef8c7f43862a893e30815e541641
christianTDConsulting <christian.berdejo@tdconsulting.es>
2023-11-28 12:04:18 +0100

    mejora de filtros

--- src/app/pages/viewEmpresasAtp/viewEmpresasAtp.module.ts
+++ src/app/pages/viewEmpresasAtp/viewEmpresasAtp.module.ts
@@ -3,12 +3,16 @@ import { CommonModule } from '@angular/common';
 import { ViewEmpresasAtpComponent } from './viewEmpresasAtp.component';
 import { TableModule } from 'primeng/table';
 import { ButtonModule } from 'primeng/button';
+import { DropdownModule } from 'primeng/dropdown';
+import { CardModule } from 'primeng/card';
 
 @NgModule({
   imports: [
     CommonModule,
     TableModule,
-    ButtonModule
+    ButtonModule,
+    DropdownModule,
+    CardModule
   ],
   declarations: [ViewEmpresasAtpComponent]
 })
